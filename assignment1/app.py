232a7fb0a4e4d65954ff7f480544c811b13c9152
Salman Mal <salmanmal@yahoo.com>
2020-09-23 13:05:34 -0700

    Assignment 1 Submission

--- assignment1/app.py
+++ assignment1/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, send_file, request
+from flask import Flask, request, send_file, request, Response, make_response
 import flask_monitoringdashboard as dashboard
 from sqlitedict import SqliteDict
 import random
 import string
 import qrcode # using pillow in the background
 from io import BytesIO
+
+# export FLASK_DEBUG=true
+# export FLASK_APP=app.py
 
 app = Flask(__name__)
 dashboard.bind(app)
@@ -16,6 +19,12 @@ def get_random_alpha_numeric(length=10):
     letters = string.ascii_lowercase
     letters+="0123456789"
     result_str = ''.join(random.choice(letters) for i in range(length))
+    
+
+    # check for duplicates
+    with SqliteDict(sqlite_db_path) as bookmarkDict:
+        if result_str in bookmarkDict:
+            return get_random_alpha_numeric()
     return result_str
 
 # Genereate QR code for bookmark url
@@ -26,6 +35,13 @@ def get_qrcode(data):
     output.seek(0)
     return output
 
+def isUrlDuplicate(url):
+    with SqliteDict(sqlite_db_path) as bookmarkDict:
+        for key in bookmarkDict:
+            if "url" in bookmarkDict[key]:
+                if bookmarkDict[key]["url"]==url:
+                    return True
+    return False
 
 stats={}
 stats_key_name="stats"
@@ -53,25 +69,29 @@ def createBookmark():
 
     url_key="url"
     if url_key in request_data:
-        with SqliteDict(sqlite_db_path) as bookmarkDict:  # note no autocommit=True
+        if isUrlDuplicate(request_data[url_key]) :
+            http_status=400
+            response={"reason": "The given URL already existed in the system."}
+        else:
+            with SqliteDict(sqlite_db_path) as bookmarkDict:  # note no autocommit=True
 
-            # Generate Alpha Numeric Unique Id
-            Id=get_random_alpha_numeric()
+                # Generate Alpha Numeric Unique Id
+                Id=get_random_alpha_numeric()
 
-            # Add new bookmark in dictonary
-            request_data["id"]=Id
-            bookmarkDict[Id] = request_data
+                # Add new bookmark in dictonary
+                request_data["id"]=Id
+                bookmarkDict[Id] = request_data
 
-            # Start stat and update stat in dictionary
-            stats[Id]=0
-            bookmarkDict[stats_key_name]=stats
+                # Start stat and update stat in dictionary
+                stats[Id]=0
+                bookmarkDict[stats_key_name]=stats
 
-            # commit changes (AUTO Commit is false )
-            bookmarkDict.commit()
+                # commit changes (AUTO Commit is false )
+                bookmarkDict.commit()
 
-            # return id in response with HTTP Status code 201
-            response={"id":Id}
-            http_status=201            
+                # return id in response with HTTP Status code 201
+                response={"id":Id}
+                http_status=201            
     else:
         # Url is not available in request body
         response={"success":False,"message":"Bookmark Url is missing in request!","data":{}}
@@ -98,7 +118,7 @@ def getBookmark(Id):
 
             http_status=200
         else :
-            response={"success":False,"message":f"Bookmark with {Id} not found in system!"}
+            response={"success":False,"message":f"Bookmark with Id {Id} not found in system!"}
             http_status=404
 
     return response,http_status
@@ -119,7 +139,7 @@ def deleteBookmark(Id):
             bookmarkDict.commit()
             http_status=204
         else :
-            response={"success":False,"message":f"Bookmark with {Id} not found in system!"}
+            response={"success":False,"message":f"Bookmark with Id {Id} not found in system!"}
             http_status=404
 
     return response,http_status
@@ -138,11 +158,26 @@ def getQRCode(Id):
             response=send_file(qrcode,mimetype='image/png')
             http_status=200
         else :
-            response={"success":False,"message":f"Bookmark with {Id} not found in system!"}
+            response={"success":False,"message":f"Bookmark with Id {Id} not found in system!"}
             http_status=404
 
     return response,http_status
 
-# @app.route('/api/bookmarks/<Id>/stats',methods=['GET'])
-# def getBookmarkStats(Id):
+@app.route('/api/bookmarks/<Id>/stats',methods=['GET'])
+def getBookmarkStats(Id):
+    response={}
+    http_status=500
+    with SqliteDict(sqlite_db_path) as bookmarkDict:
+        if Id in stats:
+            if int(request.headers.get('ETag',0))==stats[Id]:
+                http_status=304
+            else:
+                response = make_response(f"{stats[Id]}")
+                response.set_etag(f"{stats[Id]}")
+                http_status=200
+        else:
+            response={"success":False,"message":f"Bookmark with Id {Id} not found in system!"}
+            http_status=404
+    return response,http_status
+    
 
